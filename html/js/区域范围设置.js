b644a39ca795671339ad50db2d7e4477b5bde1e2
chenzewang <809555918@qq.com>
2018-10-11 18:51:29 +0800

    区域范围设置

--- html/js/区域范围设置.js
+++ html/js/区域范围设置.js
@@ -9,6 +9,7 @@ var obj=[
         statu:"多处破损",
         xy:"(12,54)",
         describe:"看起来还行应该没坏",  
+        active:false,
     },{
         num:"2",
         place:"20180611",
@@ -16,6 +17,8 @@ var obj=[
         statu:"多处破损",
         xy:"(12,54)",
         describe:"看起来还行应该没坏",  
+        active:false,
+        
     },{
         num:"2",
         place:"20180611",
@@ -23,6 +26,8 @@ var obj=[
         statu:"多处破损",
         xy:"(12,54)",
         describe:"看起来还行应该没坏",  
+        active:true,
+        
     },{
         num:"2",
         place:"20180611",
@@ -30,6 +35,8 @@ var obj=[
         statu:"多处破损",
         xy:"(12,54)",
         describe:"看起来还行应该没坏",  
+        active:true,
+        
     },{
         num:"2",
         place:"20180611",
@@ -37,6 +44,8 @@ var obj=[
         statu:"多处破损",
         xy:"(12,54)",
         describe:"看起来还行应该没坏",  
+        active:true,
+        
     },{
         num:"2",
         place:"20180611",
@@ -44,6 +53,8 @@ var obj=[
         statu:"多处破损",
         xy:"(12,54)",
         describe:"看起来还行应该没坏",  
+        active:true,
+        
     },{
         num:"2",
         place:"20180611",
@@ -51,6 +62,17 @@ var obj=[
         statu:"多处破损",
         xy:"(12,54)",
         describe:"看起来还行应该没坏",  
+        active:true,
+        
+    },{
+        num:"2",
+        place:"20180611",
+        tag:"陈某",
+        statu:"多处破损",
+        xy:"(12,54)",
+        describe:"看起来还行应该没坏", 
+        active:true,
+        
     },{
         num:"2",
         place:"20180611",
@@ -58,6 +80,8 @@ var obj=[
         statu:"多处破损",
         xy:"(12,54)",
         describe:"看起来还行应该没坏",  
+        active:true,
+        
     },{
         num:"2",
         place:"20180611",
@@ -65,13 +89,8 @@ var obj=[
         statu:"多处破损",
         xy:"(12,54)",
         describe:"看起来还行应该没坏",  
-    },{
-        num:"2",
-        place:"20180611",
-        tag:"陈某",
-        statu:"多处破损",
-        xy:"(12,54)",
-        describe:"看起来还行应该没坏",  
+        active:true,
+        
     },
 ]
 
@@ -80,18 +99,28 @@ function creatMap(ob){  //生成路线框图
     var lie=6;    
     var hang=ob.length;  
     var tab=`<table id=\"map\"> 
-                <thead><tr><th>NFC编号</th> <th>巡检区域名称</th> <th>标签类型</th> <th>状态</th> <th>位置坐标</th> <th>描述</th></tr></thead>
+                <thead><tr><th>NFC编号</th> <th>巡检区域名称</th> <th>mac</th> <th>删除</th> <th>激活</th> </tr></thead>
             `
     for(var i=0;i<hang;i++)   
     {
-        tab+="<tr>";
+        if(ob[i].active==false){
+            tab+=`<tr class="tr_noactive">`;
                 tab+=`<td>${ob[i].num}</td>`;
                 tab+=`<td>${ob[i].place}</td>`;
                 tab+=`<td>${ob[i].tag}</td>`;
-                tab+=`<td>${ob[i].statu}</td>`;
-                tab+=`<td>${ob[i].xy}</td>`;
-                tab+=`<td>${ob[i].describe}</td>`;
+                tab+=`<td><button>删除</button></td>`;
+                tab+=`<td><button class="but_noactive">激活</button></td>`;                    
+            tab+="</tr>";
+        }else{
+            tab+=`<tr class="tr_active">`;
+                tab+=`<td>${ob[i].num}</td>`;
+                tab+=`<td>${ob[i].place}</td>`;
+                tab+=`<td>${ob[i].tag}</td>`;
+                tab+=`<td><button>删除</button></td>`;
+                tab+=`<td><button class="but_active">取消激活</button></td>`;                    
         tab+="</tr>";
+        }
+        
     }           
     tab+="</table>"
     document.getElementById("graph1").innerHTML=tab;
