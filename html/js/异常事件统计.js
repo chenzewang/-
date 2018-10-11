97ff01cccd2013552b868bed2f2cb88b71c1d6e7
chenzewang <809555918@qq.com>
2018-10-11 20:23:36 +0800

    区域范围设置，异常事件统计 表格调整

--- html/js/异常事件统计.js
+++ html/js/异常事件统计.js
@@ -1,53 +1,4 @@
-// var vm=new Vue({
-//     el:"#graph1",
-//     data:{
-//         newNfc:{
-//             num:"2",
-//             event:"20180611",
-//             map:"陈某",
-//             man:"多处破损",
-//         },
-//         storage:[{
-//             num:"2",
-//             event:"20180611",
-//             map:"陈某",
-//             man:"多处破损",
-//         },{
-//             num:"2",
-//             event:"20180611",
-//             map:"陈某",
-//             man:"多处破损",
-//         },{
-//             num:"2",
-//             event:"20180611",
-//             map:"陈某",
-//             man:"多处破损",
-//         },{
-//             num:"2",
-//             event:"20180611",
-//             map:"陈某",
-//             man:"多处破损",
-//         },{
-//             num:"2",
-//             event:"20180611",
-//             map:"陈某",
-//             man:"多处破损",
-//         },{
-//             num:"2",
-//             event:"20180611",
-//             map:"陈某",
-//             man:"多处破损",
-//         },{
-//             num:"2",
-//             event:"20180611",
-//             map:"陈某",
-//             man:"多处破损",
-//         },
-//         ]
-//     },//end data
-   
-    
-// })
+
 
 
 var obj=[{
@@ -55,42 +6,56 @@ var obj=[{
     event:"炸了啊",
     map:"电气路线",
     man:"陈某",
+    xxx:"aaaa"
 },
 {
     num:"2",
     event:"炸了啊",
     map:"电气路线",
-    man:"陈某",
+    man:"陈某",    xxx:"aaaa"
+    
 },
 {
     num:"2",
     event:"炸了啊",
     map:"电气路线",
-    man:"陈某",
+    man:"陈某",    xxx:"aaaa"
+    
 },
 {
     num:"2",
     event:"炸了啊",
     map:"电气路线",
-    man:"陈某",
+    man:"陈某",    xxx:"aaaa"
+    
 },
 {
     num:"2",
     event:"炸了啊",
     map:"电气路线",
-    man:"陈某",
+    man:"陈某",    xxx:"aaaa"
+    
 },
 {
     num:"2",
     event:"炸了啊",
     map:"电气路线",
-    man:"陈某",
+    man:"陈某",    xxx:"aaaa"
+    
 },
 {
     num:"2",
     event:"炸了啊",
     map:"电气路线",
-    man:"陈某",
+    man:"陈某",    xxx:"aaaa"
+    
+},
+{
+    num:"2",
+    event:"炸了啊",
+    map:"电气路线",
+    man:"陈某",    xxx:"aaaa"
+    
 }]
 
 
@@ -99,7 +64,7 @@ function creatMap(ob){  //生成路线框图
     var lie=4;    
     var hang=ob.length;  
     var tab=`<table id=\"map\"> 
-            <thead><tr><th>序号</th> <th>事件</th> <th>路线</th> <th>巡检人</th></tr></thead>
+            <thead><tr><th>序号</th> <th>时间</th> <th>事件</th> <th>路线</th><th>巡检人</th><th>查看</th></tr></thead>
         `
     for(var i=0;i<hang;i++)   
     {
@@ -108,6 +73,8 @@ function creatMap(ob){  //生成路线框图
                 tab+=`<td>${ob[i].event}</td>`;
                 tab+=`<td>${ob[i].map}</td>`;
                 tab+=`<td>${ob[i].man}</td>`;
+                tab+=`<td>${ob[i].xxx}</td>`;
+                tab+=`<td><button> 查看</button></td>`;
         tab+="</tr>";
     }           
     tab+="</table>"
