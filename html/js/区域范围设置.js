807d86c07977491e92bcd770eba6e6be10868324
chenzewang <809555918@qq.com>
2018-11-06 19:32:45 +0800

    ie滚动条样式兼容 js文件全部用es5写法

--- html/js/区域范围设置.js
+++ html/js/区域范围设置.js
@@ -1,129 +1,121 @@
 
+var obj = [{
+    num: "2",
+    place: "20180611",
+    tag: "陈某",
+    statu: "多处破损",
+    xy: "(12,54)",
+    describe: "看起来还行应该没坏",
+    active: false
+}, {
+    num: "2",
+    place: "20180611",
+    tag: "陈某",
+    statu: "多处破损",
+    xy: "(12,54)",
+    describe: "看起来还行应该没坏",
+    active: false
 
+}, {
+    num: "2",
+    place: "20180611",
+    tag: "陈某",
+    statu: "多处破损",
+    xy: "(12,54)",
+    describe: "看起来还行应该没坏",
+    active: true
 
-var obj=[
-    {
-        num:"2",
-        place:"20180611",
-        tag:"陈某",
-        statu:"多处破损",
-        xy:"(12,54)",
-        describe:"看起来还行应该没坏",  
-        active:false,
-    },{
-        num:"2",
-        place:"20180611",
-        tag:"陈某",
-        statu:"多处破损",
-        xy:"(12,54)",
-        describe:"看起来还行应该没坏",  
-        active:false,
-        
-    },{
-        num:"2",
-        place:"20180611",
-        tag:"陈某",
-        statu:"多处破损",
-        xy:"(12,54)",
-        describe:"看起来还行应该没坏",  
-        active:true,
-        
-    },{
-        num:"2",
-        place:"20180611",
-        tag:"陈某",
-        statu:"多处破损",
-        xy:"(12,54)",
-        describe:"看起来还行应该没坏",  
-        active:true,
-        
-    },{
-        num:"2",
-        place:"20180611",
-        tag:"陈某",
-        statu:"多处破损",
-        xy:"(12,54)",
-        describe:"看起来还行应该没坏",  
-        active:true,
-        
-    },{
-        num:"2",
-        place:"20180611",
-        tag:"陈某",
-        statu:"多处破损",
-        xy:"(12,54)",
-        describe:"看起来还行应该没坏",  
-        active:true,
-        
-    },{
-        num:"2",
-        place:"20180611",
-        tag:"陈某",
-        statu:"多处破损",
-        xy:"(12,54)",
-        describe:"看起来还行应该没坏",  
-        active:true,
-        
-    },{
-        num:"2",
-        place:"20180611",
-        tag:"陈某",
-        statu:"多处破损",
-        xy:"(12,54)",
-        describe:"看起来还行应该没坏", 
-        active:true,
-        
-    },{
-        num:"2",
-        place:"20180611",
-        tag:"陈某",
-        statu:"多处破损",
-        xy:"(12,54)",
-        describe:"看起来还行应该没坏",  
-        active:true,
-        
-    },{
-        num:"2",
-        place:"20180611",
-        tag:"陈某",
-        statu:"多处破损",
-        xy:"(12,54)",
-        describe:"看起来还行应该没坏",  
-        active:true,
-        
-    },
-]
+}, {
+    num: "2",
+    place: "20180611",
+    tag: "陈某",
+    statu: "多处破损",
+    xy: "(12,54)",
+    describe: "看起来还行应该没坏",
+    active: true
 
-function creatMap(ob){  //生成路线框图
-   
-    var lie=6;    
-    var hang=ob.length;  
-    var tab=`<table id=\"map\"> 
-                <thead><tr><th>NFC编号</th> <th>巡检区域名称</th> <th>mac</th> <th>删除</th> <th>激活</th> </tr></thead>
-            `
-    for(var i=0;i<hang;i++)   
-    {
-        if(ob[i].active==false){
-            tab+=`<tr class="tr_noactive">`;
-                tab+=`<td>${ob[i].num}</td>`;
-                tab+=`<td>${ob[i].place}</td>`;
-                tab+=`<td>${ob[i].tag}</td>`;
-                tab+=`<td><button>删除</button></td>`;
-                tab+=`<td><button class="but_noactive">激活</button></td>`;                    
-            tab+="</tr>";
-        }else{
-            tab+=`<tr class="tr_active">`;
-                tab+=`<td>${ob[i].num}</td>`;
-                tab+=`<td>${ob[i].place}</td>`;
-                tab+=`<td>${ob[i].tag}</td>`;
-                tab+=`<td><button>删除</button></td>`;
-                tab+=`<td><button class="but_active">取消激活</button></td>`;                    
-        tab+="</tr>";
+}, {
+    num: "2",
+    place: "20180611",
+    tag: "陈某",
+    statu: "多处破损",
+    xy: "(12,54)",
+    describe: "看起来还行应该没坏",
+    active: true
+
+}, {
+    num: "2",
+    place: "20180611",
+    tag: "陈某",
+    statu: "多处破损",
+    xy: "(12,54)",
+    describe: "看起来还行应该没坏",
+    active: true
+
+}, {
+    num: "2",
+    place: "20180611",
+    tag: "陈某",
+    statu: "多处破损",
+    xy: "(12,54)",
+    describe: "看起来还行应该没坏",
+    active: true
+
+}, {
+    num: "2",
+    place: "20180611",
+    tag: "陈某",
+    statu: "多处破损",
+    xy: "(12,54)",
+    describe: "看起来还行应该没坏",
+    active: true
+
+}, {
+    num: "2",
+    place: "20180611",
+    tag: "陈某",
+    statu: "多处破损",
+    xy: "(12,54)",
+    describe: "看起来还行应该没坏",
+    active: true
+
+}, {
+    num: "2",
+    place: "20180611",
+    tag: "陈某",
+    statu: "多处破损",
+    xy: "(12,54)",
+    describe: "看起来还行应该没坏",
+    active: true
+
+}];
+
+function creatMap(ob) {
+    //生成路线框图
+
+    var lie = 6;
+    var hang = ob.length;
+    var tab = "<table id=\"map\"> \n                <thead><tr><th>NFC\u7F16\u53F7</th> <th>\u5DE1\u68C0\u533A\u57DF\u540D\u79F0</th> <th>mac</th> <th>\u5220\u9664</th> <th>\u6FC0\u6D3B</th> </tr></thead>\n            ";
+    for (var i = 0; i < hang; i++) {
+        if (ob[i].active == false) {
+            tab += "<tr class=\"tr_noactive\">";
+            tab += "<td>" + ob[i].num + "</td>";
+            tab += "<td>" + ob[i].place + "</td>";
+            tab += "<td>" + ob[i].tag + "</td>";
+            tab += "<td><button>\u5220\u9664</button></td>";
+            tab += "<td><button class=\"but_noactive\">\u6FC0\u6D3B</button></td>";
+            tab += "</tr>";
+        } else {
+            tab += "<tr class=\"tr_active\">";
+            tab += "<td>" + ob[i].num + "</td>";
+            tab += "<td>" + ob[i].place + "</td>";
+            tab += "<td>" + ob[i].tag + "</td>";
+            tab += "<td><button>\u5220\u9664</button></td>";
+            tab += "<td><button class=\"but_active\">\u53D6\u6D88\u6FC0\u6D3B</button></td>";
+            tab += "</tr>";
         }
-        
-    }           
-    tab+="</table>"
-    document.getElementById("graph1").innerHTML=tab;
+    }
+    tab += "</table>";
+    document.getElementById("graph1").innerHTML = tab;
 }
 creatMap(obj);
-
--- html/js/巡检报告生成.js
+++ html/js/巡检报告生成.js
@@ -1,174 +1,163 @@
+"use strict";
 
+var obj = [{
+    num: "1",
+    time: "20180911",
+    man: "陈某",
+    digest: "还行还行",
+    dian: "1",
+    statu: "无状态",
+    value: "1000000",
+    normalValue: "还行",
+    breakMessage: "还行",
+    describe: "还行",
+    project: "锅炉",
+    time1: "20180610",
+    time2: "20180612"
+}, {
+    num: "1",
+    time: "20180911",
+    man: "陈某",
+    digest: "还行还行",
+    dian: "1",
+    statu: "无状态",
+    value: "1000000",
+    normalValue: "还行",
+    breakMessage: "还行",
+    describe: "还行",
+    project: "锅炉",
+    time1: "20180610",
+    time2: "20180612"
+}, {
+    num: "1",
+    time: "20180911",
+    man: "陈某",
+    digest: "还行还行",
+    dian: "1",
+    statu: "无状态",
+    value: "1000000",
+    normalValue: "还行",
+    breakMessage: "还行",
+    describe: "还行",
+    project: "锅炉",
+    time1: "20180610",
+    time2: "20180612"
+}, {
+    num: "1",
+    time: "20180911",
+    man: "陈某",
+    digest: "还行还行",
+    dian: "1",
+    statu: "无状态",
+    value: "1000000",
+    normalValue: "还行",
+    breakMessage: "还行",
+    describe: "还行",
+    project: "锅炉",
+    time1: "20180610",
+    time2: "20180612"
+}, {
+    num: "1",
+    time: "20180911",
+    man: "陈某",
+    digest: "还行还行",
+    dian: "1",
+    statu: "无状态",
+    value: "1000000",
+    normalValue: "还行",
+    breakMessage: "还行",
+    describe: "还行",
+    project: "锅炉",
+    time1: "20180610",
+    time2: "20180612"
+}, {
+    num: "1",
+    time: "20180911",
+    man: "陈某",
+    digest: "还行还行",
+    dian: "1",
+    statu: "无状态",
+    value: "1000000",
+    normalValue: "还行",
+    breakMessage: "还行",
+    describe: "还行",
+    project: "锅炉",
+    time1: "20180610",
+    time2: "20180612"
+}, {
+    num: "1",
+    time: "20180911",
+    man: "陈某",
+    digest: "还行还行",
+    dian: "1",
+    statu: "无状态",
+    value: "1000000",
+    normalValue: "还行",
+    breakMessage: "还行",
+    describe: "还行",
+    project: "锅炉",
+    time1: "20180610",
+    time2: "20180612"
+}, {
+    num: "1",
+    time: "20180911",
+    man: "陈某",
+    digest: "还行还行",
+    dian: "1",
+    statu: "无状态",
+    value: "1000000",
+    normalValue: "还行",
+    breakMessage: "还行",
+    describe: "还行",
+    project: "锅炉",
+    time1: "20180610",
+    time2: "20180612"
+}, {
+    num: "1",
+    time: "20180911",
+    man: "陈某",
+    digest: "还行还行",
+    dian: "1",
+    statu: "无状态",
+    value: "1000000",
+    normalValue: "还行",
+    breakMessage: "还行",
+    describe: "还行",
+    project: "锅炉",
+    time1: "20180610",
+    time2: "20180612"
+}, {
+    num: "1",
+    time: "20180911",
+    man: "陈某",
+    digest: "还行还行",
+    dian: "1",
+    statu: "无状态",
+    value: "1000000",
+    normalValue: "还行",
+    breakMessage: "还行",
+    describe: "还行",
+    project: "锅炉",
+    time1: "20180610",
+    time2: "20180612"
+}];
 
-var obj=[{
-    num:"1",
-    time:"20180911",
-    man:"陈某",
-    digest:"还行还行",
-    dian:"1",
-    statu:"无状态",
-    value:"1000000",
-    normalValue:"还行",
-    breakMessage:"还行",    
-    describe:"还行",
-    project:"锅炉",
-    time1:"20180610",
-    time2:"20180612",
-},{
-    num:"1",
-    time:"20180911",
-    man:"陈某",
-    digest:"还行还行",
-    dian:"1",
-    statu:"无状态",
-    value:"1000000",
-    normalValue:"还行",
-    breakMessage:"还行",    
-    describe:"还行",
-    project:"锅炉",
-    time1:"20180610",
-    time2:"20180612",
-},{
-    num:"1",
-    time:"20180911",
-    man:"陈某",
-    digest:"还行还行",
-    dian:"1",
-    statu:"无状态",
-    value:"1000000",
-    normalValue:"还行",
-    breakMessage:"还行",    
-    describe:"还行",
-    project:"锅炉",
-    time1:"20180610",
-    time2:"20180612",
-},{
-    num:"1",
-    time:"20180911",
-    man:"陈某",
-    digest:"还行还行",
-    dian:"1",
-    statu:"无状态",
-    value:"1000000",
-    normalValue:"还行",
-    breakMessage:"还行",    
-    describe:"还行",
-    project:"锅炉",
-    time1:"20180610",
-    time2:"20180612",
-},{
-    num:"1",
-    time:"20180911",
-    man:"陈某",
-    digest:"还行还行",
-    dian:"1",
-    statu:"无状态",
-    value:"1000000",
-    normalValue:"还行",
-    breakMessage:"还行",    
-    describe:"还行",
-    project:"锅炉",
-    time1:"20180610",
-    time2:"20180612",
-},{
-    num:"1",
-    time:"20180911",
-    man:"陈某",
-    digest:"还行还行",
-    dian:"1",
-    statu:"无状态",
-    value:"1000000",
-    normalValue:"还行",
-    breakMessage:"还行",    
-    describe:"还行",
-    project:"锅炉",
-    time1:"20180610",
-    time2:"20180612",
-},{
-    num:"1",
-    time:"20180911",
-    man:"陈某",
-    digest:"还行还行",
-    dian:"1",
-    statu:"无状态",
-    value:"1000000",
-    normalValue:"还行",
-    breakMessage:"还行",    
-    describe:"还行",
-    project:"锅炉",
-    time1:"20180610",
-    time2:"20180612",
-},{
-    num:"1",
-    time:"20180911",
-    man:"陈某",
-    digest:"还行还行",
-    dian:"1",
-    statu:"无状态",
-    value:"1000000",
-    normalValue:"还行",
-    breakMessage:"还行",    
-    describe:"还行",
-    project:"锅炉",
-    time1:"20180610",
-    time2:"20180612",
-},{
-    num:"1",
-    time:"20180911",
-    man:"陈某",
-    digest:"还行还行",
-    dian:"1",
-    statu:"无状态",
-    value:"1000000",
-    normalValue:"还行",
-    breakMessage:"还行",    
-    describe:"还行",
-    project:"锅炉",
-    time1:"20180610",
-    time2:"20180612",
-},{
-    num:"1",
-    time:"20180911",
-    man:"陈某",
-    digest:"还行还行",
-    dian:"1",
-    statu:"无状态",
-    value:"1000000",
-    normalValue:"还行",
-    breakMessage:"还行",    
-    describe:"还行",
-    project:"锅炉",
-    time1:"20180610",
-    time2:"20180612",
-},
-]
+function creatGraph2(ob) {
+    //生成路线框图
 
-
-
-
-
-
-
-function creatGraph2(ob){  //生成路线框图
-    
-     var lie=5;    
-     var hang=ob.length;  
-     var tab=`<table > 
-                <thead><tr><th>送检点</th> <th>状态</th> <th>巡检值</th> <th>标准值</th> <th>故障信息描述</th></tr></thead>              
-                `
-     for(var i=0;i<hang;i++)   
-     {
-         tab+="<tr>";
-                 tab+=`<td>${ob[i].dian}</td>`;
-                 tab+=`<td>${ob[i].statu}</td>`;
-                 tab+=`<td>${ob[i].value}</td>`;
-                 tab+=`<td>${ob[i].normalValue}</td>`;
-                 tab+=`<td>${ob[i].describe}</td>`;                 
-         tab+="</tr>";
-     }           
-     tab+="</table>"
-     document.getElementById("graph2").innerHTML=tab;
- }
- creatGraph2(obj);
-
-
+    var lie = 5;
+    var hang = ob.length;
+    var tab = "<table > \n                <thead><tr><th>\u9001\u68C0\u70B9</th> <th>\u72B6\u6001</th> <th>\u5DE1\u68C0\u503C</th> <th>\u6807\u51C6\u503C</th> <th>\u6545\u969C\u4FE1\u606F\u63CF\u8FF0</th></tr></thead>              \n                ";
+    for (var i = 0; i < hang; i++) {
+        tab += "<tr>";
+        tab += "<td>" + ob[i].dian + "</td>";
+        tab += "<td>" + ob[i].statu + "</td>";
+        tab += "<td>" + ob[i].value + "</td>";
+        tab += "<td>" + ob[i].normalValue + "</td>";
+        tab += "<td>" + ob[i].describe + "</td>";
+        tab += "</tr>";
+    }
+    tab += "</table>";
+    document.getElementById("graph2").innerHTML = tab;
+}
+creatGraph2(obj);
